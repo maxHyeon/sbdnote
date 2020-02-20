4fc424d1582919232eadfe9c846da082f4d99b8a
maxHyeon <americano314@gmail.com>
2020-02-21 00:01:02 +0900

    topbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import topBar from './topBar'
 import './App.css';
-
+var topBar2 = topBar.PrimarySearchAppBar();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ${topBar2}
+      
     </div>
   );
 }
